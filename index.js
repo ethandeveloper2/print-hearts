791e0b88345d6436f1046587043b5a57b5db88e3
ethandeveloper2 <102854465+ethandeveloper2@users.noreply.github.com>
2022-12-17 17:25:33 +0900

    Revert "Feature/optimize (코드 최적화, 사용자 인터페이스 보완)"

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-import { inputGuide, printHearts } from "./functions/printHearts.js";
-import readline from "readline";
+const readline = require("readline");
 
-const inputInterface = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-inputGuide();
-inputInterface.on("line", (count) => {
-  // 입력값이 X가 아니며, 숫자일 때
-  if (count != "x" && Number(count)) {
-    console.log(`입력: ${count}`);
-    printHearts(count);
-    inputGuide();
-  }
-  // 입력값이 숫자가 아닐 때
-  if (!Number(count)) {
-    console.log("입력값을 올바르게 입력해주세요.");
-  }
-  // 입력값이 x일 때
-  if (count == "x") {
-    inputInterface.close();
-  }
+console.log("count를 입력해주세요.");
+
+rl.on("line", (line) => {
+  printHearts(line);
+  rl.close();
 });
+
+rl.on("close", () => {
+  process.exit();
+});
+
+const heart = "♡";
+
+function printHearts(count) {
+  console.log(`입력 : printHeart(${count})`);
+  console.log(`출력 :`);
+  if (count > 0) {
+    for (let i = 0; i < Math.abs(count); i++) {
+      for (let j = i; j >= 0; j--) {
+        process.stdout.write(heart);
+      }
+      console.log();
+    }
+  }
+  if (count < 0) {
+    for (let i = 0; i < Math.abs(count); i++) {
+      for (let j = i; j < Math.abs(count); j++) {
+        process.stdout.write(heart);
+      }
+      console.log();
+    }
+  }
+}
